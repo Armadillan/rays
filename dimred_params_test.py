"fix" for the scaling bug
The "fix" is to write the data to csv and read it from csv before scaling. I suspect this truncates decimals and prevents weird floating point precision errors in sklearn (possibly a bug in sklearn because the errors are different depending on how StandardScaler is applied). Currently considering whether this fix should be applied to within the scale_data function... It would decrease efficiency but reduce the amount of things the user has to consider.

dimred_params_test.py
@@ -1,5 +1,6 @@
 import os
 import time
+from io import StringIO
 
 import pandas as pd
 import numpy as np
@@ -9,6 +10,8 @@ import seaborn as sns
 import keyfi as kf
 from keyfi.dimred import UMAP
 from keyfi.cluster import HDBSCAN
+
+from sklearn.preprocessing import StandardScaler
 
 DATA_PATH = "data/postProcessing/plane"
 
@@ -22,75 +25,105 @@ df, mesh = get_data("285140.078369/data.vtk")
 #should U_z be dropped? dim=3 does not drop U_z
 cleaned_data = kf.clean_data(df, dim=2, vars_to_drop=["N2"])
 
-variables = cleaned_data.columns
-variables = ["U:0"]
 
-print("Starting embedding")
 
-#savepaths
-figpath="figures/UMAP_paramtest3d"
-embedding_path = "data/embeddings/285140.078369/test3d"
-os.makedirs(figpath, exist_ok=True)
-os.makedirs(embedding_path, exist_ok=True)
 
-saved_embeddings = os.listdir(embedding_path)
+cleaned_data = pd.read_csv(StringIO(cleaned_data.to_csv()), index_col=0)
 
-#UMAP
-n_neighbors_range = [50, 400]
-min_dist_range = [0.05, 0.1]
 
-for n_neighbors in n_neighbors_range:
-    for min_dist in min_dist_range:
 
-        start_time = time.time()
+scaled_data = kf.scale_data(
+    data=cleaned_data,
+    features=cleaned_data.columns,
+    scalers=[StandardScaler] * len(cleaned_data.columns)
+)
 
-        print(n_neighbors, min_dist)
+s2 = kf.scale_data(
+    data=cleaned_data,
+    features=[[x] for x in cleaned_data.columns],
+    scalers=[StandardScaler] * len(cleaned_data.columns)
+)
 
-        if f"{n_neighbors}_{min_dist}.npy" in saved_embeddings:
-            embedding = np.load(
-                os.path.join(embedding_path, f"{n_neighbors}_{min_dist}.npy")
-                )
-            print("loaded from existing")
 
-        else:
+s = StandardScaler().fit_transform(cleaned_data)
 
-            embedding, mapper = kf.embed_data(
-                data=cleaned_data,
-                algorithm=UMAP,
-                scale=True,
-                n_neighbors=n_neighbors,
-                min_dist=min_dist,
-                #ensures reproducibility, disable for faster compute
-                # random_state=0,
-                #how many dimensions to reduce to
-                n_components=3,
-            )
+print(np.array_equal(scaled_data.values, s))
+print(np.array_equal(scaled_data.values, s2))
+print(np.array_equal(s2, s))
 
-            np.save(
-                os.path.join(embedding_path, f"{n_neighbors}_{min_dist}"),
-                embedding
-            )
+print(scaled_data.describe())
+# print()
+# print(s)
 
-        print("creating plots")
+# variables = cleaned_data.columns
+# variables = ["U:0"]
 
-        for var in variables:
+# print("Starting embedding")
 
-            if var == "Qdot":
-                cmap_minmax=(-300, 300)
-            else:
-                cmap_minmax=[]
+# #savepaths
+# figpath="figures/UMAP_paramtest3d"
+# embedding_path = "data/embeddings/285140.078369/test3d"
+# os.makedirs(figpath, exist_ok=True)
+# os.makedirs(embedding_path, exist_ok=True)
 
-            kf.plot_embedding(
-                embedding=embedding,
-                data=cleaned_data,
-                scale_points = True,
-                cmap_var=var,
-                cmap_minmax=cmap_minmax,
-                #save=True,
-                title=f"{n_neighbors}_{min_dist}_{var}",
-                figname=f"{n_neighbors}_{min_dist}_{var}",
-                figpath=figpath,
-                view=(None, None)
-            )
+# saved_embeddings = os.listdir(embedding_path)
 
-        print(f"time: {(time.time()-start_time):.2f} s")
+# #UMAP
+# n_neighbors_range = [50, 400]
+# min_dist_range = [0.05, 0.1]
+
+# for n_neighbors in n_neighbors_range:
+#     for min_dist in min_dist_range:
+
+#         start_time = time.time()
+
+#         print(n_neighbors, min_dist)
+
+#         if f"{n_neighbors}_{min_dist}.npy" in saved_embeddings:
+#             embedding = np.load(
+#                 os.path.join(embedding_path, f"{n_neighbors}_{min_dist}.npy")
+#                 )
+#             print("loaded from existing")
+
+#         else:
+
+#             embedding, mapper = kf.embed_data(
+#                 data=cleaned_data,
+#                 algorithm=UMAP,
+#                 scale=True,
+#                 n_neighbors=n_neighbors,
+#                 min_dist=min_dist,
+#                 #ensures reproducibility, disable for faster compute
+#                 # random_state=0,
+#                 #how many dimensions to reduce to
+#                 n_components=3,
+#             )
+
+#             np.save(
+#                 os.path.join(embedding_path, f"{n_neighbors}_{min_dist}"),
+#                 embedding
+#             )
+
+#         print("creating plots")
+
+#         for var in variables:
+
+#             if var == "Qdot":
+#                 cmap_minmax=(-300, 300)
+#             else:
+#                 cmap_minmax=[]
+
+#             kf.plot_embedding(
+#                 embedding=embedding,
+#                 data=cleaned_data,
+#                 scale_points = True,
+#                 cmap_var=var,
+#                 cmap_minmax=cmap_minmax,
+#                 #save=True,
+#                 title=f"{n_neighbors}_{min_dist}_{var}",
+#                 figname=f"{n_neighbors}_{min_dist}_{var}",
+#                 figpath=figpath,
+#                 view=(None, None)
+#             )
+
+#         print(f"time: {(time.time()-start_time):.2f} s")